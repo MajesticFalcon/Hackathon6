b7af2307f669e3d2fc678cc604c56e4809c62edc
Tommy Ferguson <altferguson@gmail.com>
2016-10-22 16:06:39 -0500

    Adds routing changes

--- index.php
+++ index.php
@@ -8,18 +8,23 @@
 
     // instantiate the App object
     $app = new \Slim\Slim();
-
+	
+	
     // Add route callbacks
-    $app->get('/', function () use ($twig) {
-        echo $twig->render('index.html.twig', array());
+    $app->get('/', function () use ($twig, $profile) {
+		echo $twig->render('index.html.twig', array());
     });
-    $app->get('/profile', function () use ($twig) {
+	$app->get('/profile/:id', function ($id) use ($twig) {
+		$profile = get_profile($id);
+        echo $twig->render('ProfileEdit.html.twig', array('profile' => $profile));
+    });
+	$app->post('/profile/:id', function ($id) use ($twig) {
+		print_pre("Information Entered Completely");
         echo $twig->render('ProfileEdit.html.twig', array());
     });
-    $app->get('/update', function () use ($twig) {
-        echo $twig->render('update.html.twig', array());
+	$app->get('/search', function () use ($twig) {
+        echo $twig->render('search.html.twig', array());
     });
-
 	$app->post('/', function () use ($twig) {
        // jump("/");
     });
@@ -30,22 +35,19 @@
 
 	// print_pre($_POST);
 
-	if ($logged_in_user != ''){
-		
-	}
-	
+
 	if ($_POST['Action'] == 'Login'){
 		//Im going to let the designers worry about if the fields have been entered
 		$crypted_pass = "";
 		$crypted_pass = crypt(safe_value($_POST['password']),'$2a$09$WHYAMISTORINGTHISSALTPLAINLYINSOURCECODE?$');
-		$result = select_q("SELECT * from hackathon.users WHERE `username` = '".safe_value($_POST['username'])."' and `password` = '".$crypted_pass."'");
+		$result = id_q("SELECT * from hackathon.users WHERE `username` = '".safe_value($_POST['username'])."' and `password` = '".$crypted_pass."'");
 		if (count($result) > 0){
 			$logged_in_user = $_POST['username'];
 			print_pre("Your logged in as ".$logged_in_user."");
 			if ($result['registered'] == 1){
 				jump("org/search.php");
 			}else{
-				jump("org/register.php");
+				jump("profile/".$result['id']);
 			}
 		}else{
 			print_pre("Incorrect Login");
@@ -54,22 +56,5 @@
 		// print_pre(safe_value('s'));
 	}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
 
-    <title>St. Louis Service Share</title>
-
-    <!-- Bootstrap core CSS
-    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template 
-    <link href="dist/css/signin.css" rel="stylesheet">
 
